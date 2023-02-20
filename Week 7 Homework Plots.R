e7a2fd4f87255722960b15c71f915269b6aa17ba
Alihan-Inc <alihanince.682002@gmail.com>
2023-02-20 10:16:32 +0100

    The plot File (Final version)

--- Week 7 Homework Plots.R
+++ Week 7 Homework Plots.R
@@ -1,9 +1,10 @@
 library(tidyverse)
 library(spotifyr)
 
-df <- get_playlist_audio_features("", "7KtmJXjsDmw8WEdtyL28L3?si=21c1673ff3a14a4a")
+df <- get_playlist_audio_features("", "7KtmJXjsDmw8WEdtyL28L3?si=78d85d115b1b42c9")
 
-conditions <- c("QUARTIERS NORD", "Les dernières volontés de Mozart (Symphony)", "Cœur blanc", "Deux frères")
+conditions <- c("QUARTIERS NORD", "Les dernières volontés de Mozart (Symphony)", "Cœur blanc", "Deux frères", 
+                "L'EMPIRE DE MÉROÉ", "Dans la légende", "Capo Dei Capi Vol. II & III")
 df$language <- ifelse(df$track.album.name %in% conditions, "French", "Dutch")
 
 df_dutch <- df[df$language == "Dutch", ]
@@ -16,44 +17,86 @@ cm23 <-
   )
 
 ###############################PLOT_1###########################################
-cm23 |>                   
+cm23 |>
   mutate(
     mode = ifelse(mode == 0, "Minor", "Major")
   ) |>
-  ggplot(                     
+  ggplot(
     aes(
       x = tempo,
       y = danceability,
-      size = loudness,
+      size = energy,
       colour = mode
     )
-  ) + 
-  geom_point() +              
-  geom_rug(linewidth = 0.2) + 
-  facet_wrap(~ category) +   
-  scale_x_continuous(         
-    limits = c(50, 200),
-    breaks = c(50, 125, 200),  
-    minor_breaks = NULL      
   ) +
-  scale_y_continuous(         
+  geom_point() +
+  geom_rug(linewidth = 0.2) +
+  facet_wrap(~ category) +
+  scale_x_continuous(
+    limits = c(50, 200),
+    breaks = c(50, 125, 200),
+    minor_breaks = NULL
+  ) +
+  scale_y_continuous(
     limits = c(0, 1),
     breaks = c(0, 0.50, 1),
     minor_breaks = NULL
   ) +
-  scale_colour_brewer(       
-    type = "qual",            
-    palette = "Dark2"       
+  scale_colour_brewer(
+    type = "qual",
+    palette = "Dark2"
   ) +
-  scale_size_continuous(     
-    trans = "exp",            
-    guide = "none"            
+  scale_size_continuous(
+    trans = "exp",
+    #guide = "none",
+    range = c(0, 2)
   ) +
-  theme_bw() +             
-  labs(                       
+  theme_bw() +
+  labs(
     x = "Tempo",
     y = "Danceability",
     colour = "Mode"
-  ) + ggtitle("Relationship between tempo, danceability, loudness in French & Dutch Hip hop")
+  ) + ggtitle("Relationship between tempo, danceability, energy in French & Dutch Hip hop")
 
 ###############################PLOT_2###########################################
+ggplot(cm23, mapping = aes(x = valence, y = energy, size=loudness)) +
+  geom_boxplot(alpha = 0.5) +
+  geom_jitter(alpha = 0.5, color = "black") + 
+  facet_wrap(~ category) +
+  scale_size_continuous(
+    trans = "exp",
+    guide = "none",
+    range = c(1, 5)) +
+  ggtitle("Relationship between valence, energy and loudness in French & Dutch Hip hop")
+  theme_bw() 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
